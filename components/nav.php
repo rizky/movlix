5c470d003a81f94fd74d90f94171e69238bf52e1
Rizky Ario <rizkyario@yahoo.com>
2018-03-31 14:00:14 +0200

    Added user model and controller

--- components/nav.php
+++ components/nav.php
@@ -9,9 +9,25 @@ $cartCount = isset($_SESSION['cartCount']) ? $_SESSION['cartCount']  : "Empty";
 	<li><a href="cart.php"><i class="fa fa-shopping-cart"></i> <? echo $cartCount." Cart";?></a></li>
 	<? 
 	if (isset($_SESSION['username']) && !empty($_SESSION['username']))
-		echo '<li><i class="fa fa-user-circle-o"></i> .$_SESSION["username"].| <a href="logout.php">Logout</a></li>';
+		echo '
+		<li class="nav_account">
+			<span><i class="fa fa-user-circle-o"></i> '.$_SESSION["username"].'</span>
+			<ul class="ani">
+				<li><a href="logout.php">Logout</a></li>
+			</ul>
+		</li>
+		';
 	else
-		echo '<li><a href="login.php"><i class="fa fa-user-circle-o"></i> Account</a></li>';
+	{
+		echo '
+		<li class="nav_account">
+			<span><i class="fa fa-user-circle-o"></i> Account</span>
+			<ul class="ani">
+				<li><a href="login.php">Login</a></li>
+				<li><a href="register.php">Register</a></li>
+			</ul>
+		</li>';
+	}
 	?>
 </ul>
 </header>
