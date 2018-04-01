4a1785493e174221fc3745646cd791e3fd370f42
Rizky Ario <rizkyario@yahoo.com>
2018-04-01 19:17:21 +0200

    Updated carousel

--- index.php
+++ index.php
@@ -5,28 +5,28 @@
 	<body class="wrapper">
 		<?php include('components/nav.php'); ?>
 		<main class="landing">
-		<div id="carousel">
-			<div class="hideLeft" onclick="moveToSelected(this)">
-				<img src="https://s16.postimg.org/vklrwoxtx/cover9.jpg">
-			</div>
-			<div class="prevLeftSecond" onclick="moveToSelected(this)">
-				<img src="https://s16.postimg.org/cgsggckzp/cover8.jpg">
-			</div>
-			<div class="prev" onclick="moveToSelected(this)">
-				<img src="https://s16.postimg.org/emmrauog5/cover7.jpg">
-			</div>
-			<div class="selected" onclick="moveToSelected(this)">
-				<img src="https://s16.postimg.org/9drqcz611/cover1.jpg">
-			</div>
-			<div class="next" onclick="moveToSelected(this)">
-				<img src="https://s16.postimg.org/pnhwfvgp1/cover6.jpg">
-			</div>
-			<div class="nextRightSecond" onclick="moveToSelected(this)">
-				<img src="https://s16.postimg.org/edp6kxbnp/cover4.jpg">
-			</div>
-			<div class="hideRight" onclick="moveToSelected(this)">
-				<img src="https://s16.postimg.org/fij8qay4l/cover3.jpg">
-			</div>
+			<div id="carousel">
+				<div class="hideLeft" onclick="moveToSelected(this)">
+					<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/eF1oi474qPVJROPlSy5zyKKUwDE.jpg">
+				</div>
+				<div class="prevLeftSecond" onclick="moveToSelected(this)">
+					<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/8jEbkCRz5PFf3LzwZ505W5qJHIC.jpg">
+				</div>
+				<div class="prev" onclick="moveToSelected(this)">
+					<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/mYsoCOq82b08juHGxd3WnotiCAh.jpg">
+				</div>
+				<div class="selected" onclick="moveToSelected(this)">
+					<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/cwG6uUC9vClzWBdUjjAxloHohLq.jpg">
+				</div>
+				<div class="next" onclick="moveToSelected(this)">
+					<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg">
+				</div>
+				<div class="nextRightSecond" onclick="moveToSelected(this)">
+					<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/krQkZ4fGdyOMEpa6mBFHd3G5i0X.jpg">
+				</div>
+				<div class="hideRight" onclick="moveToSelected(this)">
+					<img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/rgxCujyIPH6K2m7sWZMPbzR78YB.jpg">
+				</div>
 			</div>
 		</main>
 		<?php include('components/footer.php'); ?>
